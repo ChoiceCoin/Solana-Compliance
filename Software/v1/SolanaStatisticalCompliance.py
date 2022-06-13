1bc1d528f30e1892959b5a4fbb1cac6c99190b4b
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-06-13 14:18:01 -0700

    Update SolanaStatisticalCompliance.py

--- Software/v1/SolanaStatisticalCompliance.py
+++ Software/v1/SolanaStatisticalCompliance.py
@@ -28,11 +28,11 @@ label5 = Label(master, text = 'Purpose', relief = 'groove', width = 12)
 label6 = Label(master, text = 'Control', relief = 'groove', width = 12)
 # Factor 8 Derivatives: An asset is less likely to be a security if it does not offer users derivatives or returns.
 label7 = Label(master, text = 'Derivatives', relief = 'groove', width = 12)
-# Factor 9 Common Enterprise: An asset is less likely to be a security if it is not dedicated to the furtherance of a common enterprise.
-label8 = Label(master, text = 'Common Enterprise', relief = 'groove', width = 12)
+# Factor 9 Commonality: An asset is less likely to be a security if it is not dedicated to the furtherance of a common enterprise.
+label8 = Label(master, text = 'Commonality', relief = 'groove', width = 12)
 # Compliance Score provides a statistical measure of the likelyhood an asset is a security.
 # The statistical analysis is set such that 1.0 is least likely to be a security and 0.0 is most likely to be a security.
-label8 = Label(master, text = 'Compliance Score', relief = 'groove', width = 20)
+label9 = Label(master, text = 'Compliance Score', relief = 'groove', width = 20)
 entry0 = Entry(master, relief = 'groove', width = 12)
 entry1 = Entry(master, relief = 'groove', width = 12)
 entry2 = Entry(master, relief = 'groove', width = 12)
@@ -48,13 +48,11 @@ blank0 = Entry(master, relief = 'groove', width = 20)
 # The backend is an artificial intelligence (AI) program for automating compliance analysis.
 # The AI program uses an embedded knowledge and a geometric mean to capture human intuition and provide intelligent analysis.
 def compliance_ai():
-    intuitive_ratio = 1/9
-    embedded_knowledge = float(mydict[entry0.get()]) * float(mydict[entry1.get()]) * float(mydict[entry2.get()]) *
-    float( mydict[entry3.get()]) * float(mydict[entry4.get()]) * float(mydict[entry5.get()]) *
-    float(mydict[entry6.get()]) * float(mydict[entry7.get()] * float(mydict[entry8.get()])
-    intelligent_analysis = embedded_knowledge ** intuitive_ratio
-    blank0.insert(0, intelligent_analysis)
-button0 = Button(master, text = 'Calculate SPL Token Compliance', relief = 'groove', width = 20, command =compliance_ai)
+    intuition = 1/9
+    knowledge = float(natural_language_compression[entry0.get()]) * float(natural_language_compression[entry1.get()]) * float(natural_language_compression[entry2.get()]) *float(natural_language_compression[entry3.get()]) * float(natural_language_compression[entry4.get()]) * float(natural_language_compression[entry5.get()]) * float(natural_language_compression[entry6.get()]) * float(natural_language_compression[entry7.get()]) * float(natural_language_compression[entry8.get()])
+    intelligence = float(knowledge) ** intuition
+    blank0.insert(0, intelligence)
+button0 = Button(master, text = 'Calculate SPL Compliance', relief = 'groove', width = 25, command =compliance_ai)
 
 # Frontend
 ## The frontend is a simple user interface.
@@ -67,6 +65,7 @@ label5.grid( row = 6, column = 1, padx = 10 )
 label6.grid( row = 7, column = 1, padx = 10 )
 label7.grid( row = 8, column = 1, padx = 10 )
 label8.grid( row = 9, column = 1, padx = 10 )
+label9.grid( row = 2, column = 3, padx = 10 )
 entry0.grid( row = 1, column = 2, padx = 10 )
 entry1.grid( row = 2, column = 2, padx = 10 )
 entry2.grid( row = 3, column = 2, padx = 10 )
