d180120ee012ea6d4668b175c3c2abf9bc07138d
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-06-13 14:32:15 -0700

    Add files via upload

---
+++ Software/v1/SolanaStatisticalCompliance.js
@@ -0,0 +1,7 @@
+
+function knowledge(f1, f2, f3, f4, f5, f6, f7, f8, f9) {
+  return f1 * f2 * f3 * f4 * f5 * f6 * f7 * f8 * f9;   
+}
+function intelligence(knowledge, f10){
+    return knowledge ** f10
+}
